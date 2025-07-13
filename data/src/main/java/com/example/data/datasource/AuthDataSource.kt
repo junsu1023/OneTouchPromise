4b4039ea8454a8ab0187641771c161c444558418
KimJunsu <12jangdokg55@gmail.com>
2025-07-13 17:06:05 +0900

    이미지 저장 datasource 로직 구현

--- data/src/main/java/com/example/data/datasource/AuthDataSource.kt
+++ data/src/main/java/com/example/data/datasource/AuthDataSource.kt
@@ -1,25 +1,38 @@
 package com.example.data.datasource
 
+import android.net.Uri
 import com.example.data.entity.UserEntity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
 import kotlinx.coroutines.tasks.await
 import kotlin.coroutines.cancellation.CancellationException
 
 class AuthDataSource(
     private val firebaseAuth: FirebaseAuth
 ) {
-    suspend fun signUp(email: String, nickname: String, password: String): Result<UserEntity> = try {
+    suspend fun signUp(
+        email: String,
+        nickname: String,
+        password: String,
+        profileImageUri: Uri
+    ): Result<UserEntity> = try {
         val user = firebaseAuth.createUserWithEmailAndPassword(email, password).await().user
             ?: throw IllegalStateException("User Signup Failed")
 
         val uid = user.uid
+        val imageRef = Firebase.storage.reference.child("profile_images/$uid.jpg")
+        imageRef.putFile(profileImageUri).await()
+
+        val imageUrl = imageRef.downloadUrl.await().toString()
+
         val userMap = mapOf(
             "uid" to uid,
             "email" to email,
             "nickname" to nickname,
-            "password" to password
+            "password" to password,
+            "profileImageUrl" to imageUrl
         )
 
         Firebase.firestore.collection("users").document(uid).set(userMap).await()
--- data/src/main/java/com/example/data/entity/UserEntity.kt
+++ data/src/main/java/com/example/data/entity/UserEntity.kt
@@ -3,5 +3,6 @@ package com.example.data.entity
 data class UserEntity(
     val uid: String,
     val email: String?,
-    val nickname: String?
+    val nickname: String?,
+    val profileImageUrl: String? = null
 )
