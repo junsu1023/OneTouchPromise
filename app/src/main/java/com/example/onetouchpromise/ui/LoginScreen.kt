1c4fa1113fd7921a8e5cbb3b8f5109f925d0c452
KimJunsu <12jangdokg55@gmail.com>
2025-07-13 19:47:22 +0900

    불필요한 import 제거

--- app/src/main/java/com/example/onetouchpromise/ui/LoginScreen.kt
+++ app/src/main/java/com/example/onetouchpromise/ui/LoginScreen.kt
@@ -1,8 +1,6 @@
 package com.example.onetouchpromise.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
