879f9ac23317ec3a8eff7662412f0f456d62b401
KimJunsu <12jangdokg55@gmail.com>
2025-07-13 18:40:44 +0900

    이미지 포기

--- data/src/main/java/com/example/data/datasource/AuthDataSource.kt
+++ data/src/main/java/com/example/data/datasource/AuthDataSource.kt
@@ -15,24 +15,18 @@ class AuthDataSource(
     suspend fun signUp(
         email: String,
         nickname: String,
-        password: String,
-        profileImageUri: Uri
+        password: String
     ): Result<UserEntity> = try {
         val user = firebaseAuth.createUserWithEmailAndPassword(email, password).await().user
             ?: throw IllegalStateException("User Signup Failed")
 
         val uid = user.uid
-        val imageRef = Firebase.storage.reference.child("profile_images/$uid.jpg")
-        imageRef.putFile(profileImageUri).await()
-
-        val imageUrl = imageRef.downloadUrl.await().toString()
 
         val userMap = mapOf(
             "uid" to uid,
             "email" to email,
             "nickname" to nickname,
-            "password" to password,
-            "profileImageUrl" to imageUrl
+            "password" to password
         )
 
         Firebase.firestore.collection("users").document(uid).set(userMap).await()
