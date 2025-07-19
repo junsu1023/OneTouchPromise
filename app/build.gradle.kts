14d40d2096eb0509b995a5f905a700ad6369f24c
KimJunsu <12jangdokg55@gmail.com>
2025-07-19 14:55:52 +0900

    FCM 사용하기 위한 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.messaging)
 
     // hilt
     implementation(libs.hilt.android)
